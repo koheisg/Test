25e4bb0c6c168bd59a49fdc1aa8c25432b1eff4e
kassy916 <warallyinfo@gmail.com>
2018-06-18 17:57:46 +0900

    event info

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,11 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :update, :edit, :destroy]
   before_action :set_current_user, only: [:index, :show, :update, :edit, :destroy]
+  before_action :set_remote_ip, only: [:create, :update]
 
-	def new
+  def new
       # イベントを登録
-　　     @event = Event.new
+        @event = Event.new
 
       # 出演者を登録
         @event.event_performers.build
@@ -15,17 +16,11 @@ class EventsController < ApplicationController
       # カテゴリを登録
         @event.event_categories.build
 
-	end
+  end
 
-	def create
+  def create
         # イベント情報を取得
          @event = Event.new(event_params)
-
-        # ipアドレスを取得
-         @remote_ip = request.remote_ip
-
-        # ログインユーザーを取得
-         @user_id = current_user.id
 
         # DB保存→詳細画面へリダイレクト
         if @event.save
@@ -37,129 +32,118 @@ class EventsController < ApplicationController
             flash.now[:error] = 'ライブ登録に失敗しました...。お手数ですが最初からやり直してください。'
             render :new
         end
-    end
+  end
 
-    def index
-        @q = Event.ransack(params[:q])
-    	@events = Event.page(params[:page]).includes(:event_performers, :event_links, :event_categories).reverse_order
-    end
+  def index
+    @events = Event.page(params[:page]).includes(:event_performers, :event_links, :event_categories).reverse_order
+  end
 
-    def search
-        @date = params[:date]
-        @q = params[:q]
+  def search
+      @date = params[:date]
+      @q = params[:q]
 
-        #binding.pry
+      #binding.pry
 
-        #ライブ情報を日付で検索
-        return @events = SearchDatetimeService.new(@date).execute if @date.present?
+      #ライブ情報を日付で検索
+      return @events = SearchDatetimeService.new(@date).execute if @date.present?
 
-        #日付もしくはキーワードで検索
-        return @events = SearchDatetimeKeywordService.new(params[:datetime],params[:q]).execute if params[:datetime].present? && params[:q].present?
-        return @events = SearchKeywordService.new(params[:q]).execute if params[:q].present?
-        return @events = SearchDatetimeService.new(params[:datetime]).execute if params[:datetime].present?
+      #日付もしくはキーワードで検索
+      return @events = SearchDatetimeKeywordService.new(params[:datetime],params[:q]).execute if params[:datetime].present? && params[:q].present?
+      return @events = SearchKeywordService.new(params[:q]).execute if params[:q].present?
+      return @events = SearchDatetimeService.new(params[:datetime]).execute if params[:datetime].present?
 
-        binding.pry
+      binding.pry
 
-        respond_to do |format|
-          format.html # index.html.erb
-        end
-    end
+      respond_to do |format|
+        format.html # index.html.erb
+      end
+  end
 
-    def show
-    end
+  def show
+  end
 
-    def edit
-        @event = Event.find_by(params[:id])
+  def edit
 
-      # 出演者を編集
-        @event.event_performers.build
+  end
 
-      # リンクを編集
-        @event.event_links.build
+  def update
+      # エラーチェック＆DB保存→詳細画面へリダイレクト
+      if @event.update(event_params)
+          # イベントが編集されたら、変更履歴テーブルを更新
+          @event_id = @event.id
+          UpdateEventChangeHistoryService.new(@event.id,@remote_ip,@user_id).execute
 
-      # カテゴリを編集
-        @event.event_categories.build
-    end
+          redirect_to event_path(params[:id]) , notice: 'ありがとうございます！ライブ登録が完了しました！'
+      else
+          flash.now[:error] = 'ライブ編集に失敗しました...。お手数ですが最初からやり直してください。'
+          render :edit
+      end
+  end
 
-    def update
-        @event = Event.find_by(params[:id])
+  def destroy
+      @event.destroy
+      redirect_to events_path
+  end
 
-        # ipアドレスを取得
-         @remote_ip = request.remote_ip
+private
+  #ライブ情報
+  def event_params
+      params.require(:event)
+      .permit(
+          :id,
+          :datetime,
+          :title,
+          :description,
+          :tel,
+          :email,
+          :image,
+          event_links_attributes: [
+              :id,
+              :event_id,
+              :url],
+          event_change_histories_attributes: [
+              :id,
+              :event_id,
+              :user_id,
+              :user_ip],
+          event_performers_attributes: [
+              :id,
+              :event_id,
+              :performer],
+          event_categories_attributes: [
+              :id,
+              :event_id,
+              :category]
+      )
+  end
 
-        # ログインユーザーを取得
-         @user_id = current_user.id
+  #ユーザー情報
+  def user_params
+      params.require(:user).permit(:id, :name, :profile_image, :uid, :email, :password)
+  end
 
-        # エラーチェック＆DB保存→詳細画面へリダイレクト
-        if @event.update(event_params)
-            # イベントが編集されたら、変更履歴テーブルを更新
-            @event_id = @event.id
-            UpdateEventChangeHistoryService.new(@event.id,@remote_ip,@user_id).execute
+  #検索クエリ
+  def search_params
+      params.require(:q).permit(:title_or_description_or_event_performers)
+      params.require(:keyword).permit(:title_or_description)
+      params.require(:date).permit(:datetime_eq)
+      params.require(:date_and_keyword).permit
+  end
 
-            redirect_to event_path(params[:id]) , notice: 'ありがとうございます！ライブ登録が完了しました！'
-        else
-            flash.now[:error] = 'ライブ編集に失敗しました...。お手数ですが最初からやり直してください。'
-            render :edit
-        end
-    end
+  def set_event
+      @event = Event.find_by!(id: params[:id])
+  end
 
-    def destroy
-        @event.destroy
-        redirect_to events_path
-    end
+  def set_current_user
+      if current_user.present?
+          @user = current_user
+          # ログインユーザーを取得
+          @user_id = current_user.id
+      end
+  end
 
-	private
-    #ライブ情報
-    def event_params
-        params.require(:event)
-        .permit(
-            :id,
-            :datetime,
-            :title,
-            :description,
-            :tel,
-            :email,
-            :image,
-            event_links_attributes: [
-                :id,
-                :event_id,
-                :url],
-            event_change_histories_attributes: [
-                :id,
-                :event_id,
-                :user_id,
-                :user_ip],
-            event_performers_attributes: [
-                :id,
-                :event_id,
-                :performer],
-            event_categories_attributes: [
-                :id,
-                :event_id,
-                :category]
-        )
-    end
-
-    #ユーザー情報
-    def user_params
-        params.require(:user).permit(:id, :name, :profile_image, :uid, :email, :password)
-    end
-
-    #検索クエリ
-    def search_params
-        params.require(:q).permit(:title_or_description_or_event_performers)
-        params.require(:keyword).permit(:title_or_description)
-        params.require(:date).permit(:datetime_eq)
-        params.require(:date_and_keyword).permit
-    end
-
-    def set_event
-        @event = Event.find_by!(id: params[:id])
-    end
-
-    def set_current_user
-        if current_user.present?
-            @user = current_user
-        end
-    end
+  def set_remote_ip
+     # ipアドレスを取得
+       @remote_ip = request.remote_ip
+  end
 end
