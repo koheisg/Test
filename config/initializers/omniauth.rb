6dd75767985d983a71036f9bf685aedadc341c7e
kassy916 <warallyinfo@gmail.com>
2018-06-19 00:05:28 +0900

    twitter auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  #provider :twitter, ENV["API_KEY"], ENV["API_SECRET"]
+  provider :twitter, ENV["TWITTER_API_KEY"], ENV["TWITTER_API_SECRET"], callback_url: "	https://127.0.0.1:3000/users/auth/twitter/callback"
 end
