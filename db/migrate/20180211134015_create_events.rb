918690c1e6345fc318bfba01c6fc2913f00945e8
kassy916 <warallyinfo@gmail.com>
2018-06-18 18:44:32 +0900

    event info update

--- db/migrate/20180211134015_create_events.rb
+++ db/migrate/20180211134015_create_events.rb
@@ -1,16 +1,11 @@
 class CreateEvents < ActiveRecord::Migration[5.1]
   def change
     create_table :events do |t|
-      t.integer :live_id
       t.date :live_date
       t.time :live_start
-      t.string :live_name
-      t.string :live_remarks
-      t.string :link_1
-      t.string :link_2
-      t.string :link_3
-      t.string :link_4
-      t.string :live_email
+      t.string :title
+      t.string :description
+      t.string :tel
 
       t.timestamps
     end
