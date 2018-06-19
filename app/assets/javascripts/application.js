5e28f92bc52f656f896dde03169e3f0f5e7add17
kassy916 <warallyinfo@gmail.com>
2018-06-20 01:37:21 +0900

    add _event_info

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,6 +3,12 @@
 //= require jquery_ujs
 //= require turbolinks
 
+// flatpickrを起動させるinputのID *classでも可
+flatpickr('#EventCalendar');
+
+//jQueryでflatpickrを使用
+$("#EventCalender").flatpickr(altInput: true,altFormat: "F j, Y",dateFormat: "Y-m-d",);
+
  var toJPN = {
                weekdays : ['日', '月', '火', '水', '木', '金', '土'],
                months: ['1月', '2月', '3月', '4月', '5月', '6月', '7月',
@@ -16,8 +22,3 @@ flatpickr.init.prototype.l10n.weekdays.shorthand = toJPN.weekdays;
 // '月'を日本語化
 flatpickr.init.prototype.l10n.months.longhand = toJPN.months;
 
-// flatpickrを起動させるinputのID *classでも可
-flatpickr('#yourCalendar');
-altInput: true,
-altFormat: "F j, Y",
-dateFormat: "Y-m-d",
