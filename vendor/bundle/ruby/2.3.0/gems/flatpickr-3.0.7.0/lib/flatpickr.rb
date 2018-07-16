144cc482a8b69b923ebb49b629671fd011edca50
kassy916 <warallyinfo@gmail.com>
2018-07-17 04:43:34 +0900

    ruby 2.3.1 degrade

--- vendor/bundle/ruby/2.3.0/gems/flatpickr-3.0.7.0/lib/flatpickr.rb
+++
@@ -1,4 +0,0 @@
-require 'flatpickr/version'
-require 'flatpickr/engine' if (defined?(Rails) && defined?(Rails::Engine))
-
-module Flatpickr; end
--- vendor/bundle/ruby/2.3.0/gems/flatpickr-3.0.7.0/lib/flatpickr/engine.rb
+++
@@ -1,4 +0,0 @@
-module Flatpickr
-  class Engine < Rails::Engine
-  end
-end
